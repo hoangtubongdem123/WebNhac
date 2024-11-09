56f8c90f4f9b4eb87c6d49d0ed056806ea6bc323
Tung <uctung43@gmail.com>
2024-11-09 20:11:03 +0700

    vcc

---
+++ Test1/Controllers/PlaylistController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Test1.Controllers
+{
+    public class PlaylistController : Controller
+    {
+        // GET: Playlist
+        public ActionResult Playlist()
+        {
+            return View();
+        }
+    }
+}
